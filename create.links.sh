7654e4036ea1c8408a66d11a5229760c7bcdeefb
Douglas Bastos <douglashsb@gmail.com>
2017-02-08 13:54:33 -0200

    Cria script para criação dos links simbolicos

---
+++ create.links.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "Iniciando criação de links"
+
+ln -sf ~/workspace/configs/dotfiles/.profile ~/.profile
+ln -sf ~/workspace/configs/sublime/preferences ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -sf ~/workspace/configs/sublime/SublimeLinter ~/.config/sublime-text-3/Packages/User/SublimeLinter
+ln -sf ~/workspace/configs/sublime/keymap ~/.config/sublime-text-3/Packages/User/Default\ (Linux).sublime-keymap
+ln -sf ~/workspace/configs/sublime/plugins ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
+
+echo "Finalizada criação de links"
