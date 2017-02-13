2112f54563c940998dea601e1bf8365c478f7658
Douglas Bastos <douglashsb@gmail.com>
2017-02-13 11:29:06 -0200

    Inclui configurações do terminator

--- create.links.sh
+++ create.links.sh
@@ -6,5 +6,6 @@ ln -sf ~/workspace/configs/sublime/preferences ~/.config/sublime-text-3/Packages
 ln -sf ~/workspace/configs/sublime/SublimeLinter ~/.config/sublime-text-3/Packages/User/SublimeLinter
 ln -sf ~/workspace/configs/sublime/keymap ~/.config/sublime-text-3/Packages/User/Default\ (Linux).sublime-keymap
 ln -sf ~/workspace/configs/sublime/plugins ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
+ln -sf ~/workspace/configs/terminator ~/.config/terminator
 
 echo "Finalizada criação de links"
