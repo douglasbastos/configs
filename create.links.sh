e61324519ab0aab01a0ccc330f5147920e7fc3e9
Douglas Bastos <douglashsb@gmail.com>
2017-06-20 16:52:05 -0300

    Remove comando de para adicionar link simbolico na keymap do sublime

--- create.links.sh
+++ create.links.sh
@@ -4,7 +4,6 @@ echo "Iniciando criação de links"
 ln -sf ~/workspace/configs/dotfiles/.profile ~/.profile
 ln -sf ~/workspace/configs/sublime/preferences ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 ln -sf ~/workspace/configs/sublime/SublimeLinter ~/.config/sublime-text-3/Packages/User/SublimeLinter
-ln -sf ~/workspace/configs/sublime/keymap ~/.config/sublime-text-3/Packages/User/Default\ (Linux).sublime-keymap
 ln -sf ~/workspace/configs/sublime/plugins ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
 ln -sf ~/workspace/configs/terminator ~/.config/terminator
 
